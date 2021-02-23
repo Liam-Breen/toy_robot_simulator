847573629189c6e75e42cb37eada3b2a5c113fe5
Liam-Breen <liam.breen25@gmail.com>
2021-02-23 22:33:28 +1100

    added the move command

--- toy_robot.py
+++ toy_robot.py
@@ -32,6 +32,29 @@ class ToyRobot():
             self.current_coordinates = coordinates
             self.current_direction = direction
 
+    def move(self) -> None:
+        """Moves the toy robot forward one spot depending on the direction it faces"""
+
+        if self.current_direction == 'EAST':
+            x_cord = self.current_coordinates[0]
+            if x_cord + 1 < 5:
+                self.current_coordinates = (x_cord + 1, self.current_coordinates[1])
+
+        elif self.current_direction == 'WEST':
+            x_cord = self.current_coordinates[0]
+            if x_cord - 1 < 0:
+                self.current_coordinates = (x_cord - 1, self.current_coordinates[1])
+
+        elif self.current_direction == 'NORTH':
+            y_cord = self.current_coordinates[1]
+            if y_cord + 1 < 5:
+                self.current_coordinates = (self.current_coordinates[0], y_cord + 1)
+
+        elif self.current_direction == 'SOUTH':
+            y_cord = self.current_coordinates[1]
+            if y_cord + 1 < 0:
+                self.current_coordinates = (self.current_coordinates[0], y_cord - 1)
+
 if __name__ == "__main__":
     debug = True
     toy_robot = None
@@ -40,9 +63,12 @@ if __name__ == "__main__":
     while command:
 
         if 'PLACE' in command:
+
             if debug: command = 'PLACE 0,0,NORTH'
+
             if toy_robot:
                 toy_robot.place(command)
+
             else:
                 toy_robot = ToyRobot()
                 toy_robot.place(command)
@@ -50,7 +76,13 @@ if __name__ == "__main__":
                 if debug: assert toy_robot.current_direction == 'NORTH'
 
         elif toy_robot:
+
             if command == 'REPORT':
                 print(toy_robot.report())
+
+            elif command == 'MOVE':
+                toy_robot.move()
+                if debug: assert toy_robot.current_coordinates == (0, 1)
+                if debug: assert toy_robot.current_direction == 'NORTH'
 
         command = input("Enter your command: ")
