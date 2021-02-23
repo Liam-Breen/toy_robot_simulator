f259fa48192e94fa839ca1dad95fc3502cd24ef2
Liam-Breen <liam.breen25@gmail.com>
2021-02-23 22:09:48 +1100

    Created the report command

--- toy_robot.py
+++ toy_robot.py
@@ -4,5 +4,8 @@ class ToyRobot():
         self.current_coordinates = None
         self.current_direction = None
 
+    def report(self) -> tuple:
+        return f"{self.current_coordinates[0]},{self.current_coordinates[1]},{self.current_direction}"
+
 if __name__ == "__main__":
     ...
